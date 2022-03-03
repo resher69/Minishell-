3cfe1ddb1ca173bacac2ac7d2dc2747f814e106b
Arnaud Gardet <agardet@z4r2p3.42lyon.fr>
2022-03-03 15:16:42 +0100

    norminette

--- inc/minishell.h
+++ inc/minishell.h
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agardet <agardet@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/03 15:14:39 by agardet           #+#    #+#             */
+/*   Updated: 2022/03/03 15:15:23 by agardet          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
@@ -11,28 +22,26 @@
 
 # include "utils.h"
 
-typedef struct 	s_cmd
+typedef struct s_cmd
 {
-	char    **av;
-	size_t  ac;
-}       		t_cmd;
+	char	**av;
+	size_t	ac;
+}				t_cmd;
 
 typedef struct s_env_var
 {
-	char	*name;
-	char	*value;
-
+	char				*name;
+	char				*value;
 	struct s_env_var	*next;
 }				t_env_var;
 
-typedef struct	s_shell
+typedef struct s_shell
 {
 	t_cmd	**usr_cmd;
 	size_t	n_cmd;
 }				t_shell;
 
-
-extern 	t_env_var	*g_env;
+extern t_env_var	*g_env;
 
 void	init_env(char **envp);
 char	*get_env(char *var_name);
--- inc/utils.h
+++ inc/utils.h
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agardet <agardet@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/03 15:15:30 by agardet           #+#    #+#             */
+/*   Updated: 2022/03/03 15:16:03 by agardet          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef UTILS_H
 # define UTILS_H
@@ -8,17 +19,17 @@
 # define QUOTE_SINGLE 1
 # define QUOTE_DOUBLE 2
 
-int         get_quote(char c);
+int			get_quote(char c);
 
 size_t		ft_strcmp(char *a, char *b);
-size_t      ft_strlen(char *str);
-size_t      ft_strchr(char *s, char c);
-size_t      ft_bzero(char *s, size_t size);
-size_t      ft_bcopy(char *dst, char *src, size_t size);
+size_t		ft_strlen(char *str);
+size_t		ft_strchr(char *s, char c);
+size_t		ft_bzero(char *s, size_t size);
+size_t		ft_bcopy(char *dst, char *src, size_t size);
 char		*ft_strldup(char *s, size_t size);
 void		ft_strlcpy(char *dst, char *src, size_t len);
 
-int     	is_ifs(char c);
-int	        is_valid_var_char(char c);
-int         skip_ifs(char *str);
+int			is_ifs(char c);
+int			is_valid_var_char(char c);
+int			skip_ifs(char *str);
 #endif
