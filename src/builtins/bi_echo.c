2cc1c1707a78894898dc4d5e9d4b52816c5e788c
Arnaud Gardet <agardet@z2r6p2.42lyon.fr>
2022-03-11 15:13:23 +0100

    echo and env done' '

--- src/builtins/bi_echo.c
+++ src/builtins/bi_echo.c
@@ -1,8 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bi_echo.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agardet <agardet@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/11 12:17:31 by agardet           #+#    #+#             */
+/*   Updated: 2022/03/11 12:17:34 by agardet          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-void	bi_echo(char **av, t_shell *shell)
+static int	check_option_n(char *arg)
 {
-	puts("echo");
-	
-	;
+	size_t	i;
+
+	i = 0;
+	while (arg[i])
+	{
+		if (arg[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	bi_echo(char **av, int fd)
+{
+	size_t	i;
+	bool	option;
+
+	//g_error_number = EXIT_SUCCESS;
+	i = 0;
+	option = 0;
+	if (av[1])
+	{
+		while (av[++i] && av[i][0] == '-')
+		{
+			if (check_option_n(av[i] + 1))
+				option = 1;
+			else
+				break ;
+		}
+		while (av[i])
+		{
+			ft_putstr_fd(av[i], fd);
+			if (av[++i])
+				ft_putchar_fd(' ', fd);
+		}
+	}
+	if (!option)
+		ft_putchar_fd('\n', fd);
 }
--- src/builtins/bi_env.c
+++ src/builtins/bi_env.c
@@ -1,7 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bi_env.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agardet <agardet@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/11 12:29:10 by agardet           #+#    #+#             */
+/*   Updated: 2022/03/11 14:53:48 by agardet          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-void	bi_env(t_shell *shell)
+void	bi_env(t_shell *shell, int fd)
 {
-	puts("env");
-	;
+	t_env_var	*elem;
+
+	//g_error_number = EXIT_SUCCESS;
+	elem = *shell->env;
+	while (elem)
+	{
+		if (elem->value)
+		{
+			ft_putstr_fd(elem->name, fd);
+			ft_putstr_fd(elem->value, fd);
+			ft_putstr_fd("\n", fd);
+		}
+		elem = elem->next;
+	}
 }
